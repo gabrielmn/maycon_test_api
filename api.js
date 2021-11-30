05de4da47d70cc3c9959b37f270e969b74b77225
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-11-30 16:33:18 -0300

    FIX - load json parser before loading routes.

--- api.js
+++ api.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(require("./routes/routes"))
 app.use(express.json());
-
+app.use(require("./routes/routes"))
 
 app.listen(3000,()=>{
     console.log('API online');
