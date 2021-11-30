50b68f4bf72820d94b3ee124d5b8a61afcf5a2eb
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-11-30 18:06:12 -0300

    FIX - products and categories createdAt and updatedAt values.

--- routes/controllers/product.js
+++ routes/controllers/product.js
@@ -12,11 +12,41 @@ router.post("/", auth, async (req, res, next) => {
 
     let connection;
     try {
+        const currentDate = new Date().toISOString().slice(0,10);
         const {category_id, name, image } = req.body;
         const image_path = `${LOCAL_STORAGE}/product_${name}.jpg`;
         await fs.writeFile(image_path, image, 'base64')
-        let sql = 'INSERT INTO `products`(`category_id`, `name`, `image_path`) VALUES (?, ?, ?);'
-        sql = mysql.formatQuery(sql, [category_id, name, image_path]);
+        let sql = 'INSERT INTO `products`(`category_id`, `name`, `image_path`, `createdAt`, `updatedAt`) VALUES (?, ?, ?, ?, ?);'
+        sql = mysql.formatQuery(sql, [category_id, name, image_path, currentDate, currentDate]);
+        connection = await mysql.connect();
+        const [rows, fields] = await connection.query(sql);
+        if (rows.affectedRows  === 1) {
+            // product added successfully
+            res.status(201).send({
+                product_id: rows.insertId
+            });
+        } else {
+                res.status(400).send("Something went wrong.:(");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    } finally {
+        mysql.close(connection);
+    }
+})
+
+
+router.post("/category", auth, async (req, res, next) => {
+
+    let connection;
+    try {
+        const currentDate = new Date().toISOString().slice(0,10);
+        const {name, image } = req.body;
+        const image_path = `${LOCAL_STORAGE}/category_${name}.jpg`;
+        await fs.writeFile(image_path, image, 'base64')
+        let sql = 'INSERT INTO `categories`(`name`, `image_path`,`createdAt`, `updatedAt`) VALUES ( ?, ?, ?, ?);'
+        sql = mysql.formatQuery(sql, [ name, image_path, currentDate, currentDate]);
         connection = await mysql.connect();
         const [rows, fields] = await connection.query(sql);
         if (rows.affectedRows  === 1) {
