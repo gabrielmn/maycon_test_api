bc9776ce4ef12e96c767baaf3c81243e9fcb3384
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-11-30 17:18:30 -0300

    Load product endpoints.

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/user', require("./controllers/user"));
+router.use('/product', require("./controllers/product"));
 
 module.exports = router;
