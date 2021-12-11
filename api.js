b773b29427955f868e4df8a399d110e255cac70b
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-12-11 00:37:45 -0300

    Added cors.

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 const express = require('express');
+var cors = require('cors');
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(require("./routes/routes"))
 
-app.listen(3000,()=>{
+app.listen(3001,()=>{
     console.log('API online');
 })
