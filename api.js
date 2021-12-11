c9c1e4e62c4bbb281caa15aab03824d96a18eabf
Gabriel M. Nori <gabriel.m.nori@hotmail.com>
2021-12-11 03:30:25 -0300

    Increase upload limit.

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 
 const app = express();
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(require("./routes/routes"))
 
 app.listen(3001,()=>{
